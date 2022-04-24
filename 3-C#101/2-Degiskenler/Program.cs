b711a1cf7e5ecc17d9a43b9b288992dd6702fdfa
Bahtiyar Bayir <bhtyr.byr@gmail.com>
2022-04-24 16:12:32 +0300

    C#101 Eğitimi Ders 1 - Değişkenler

---
+++ 3-C#101/2-Degiskenler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace degiskenler
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string dt_str = DateTime.Now.ToString("dd MMMM yyyy HH:dd");
+
+            Console.WriteLine(dt_str);
+
+        }
+    }
+}
--- 3-C#101/README.md
+++ 3-C#101/README.md
@@ -1,3 +1,4 @@
 # C#101 Eğitimi
 
 Ders 1 - [İlk Console Uygulaması](1-ConsoleApp/)
+Ders 2 - [Değişkenler](2-Degiskenler/)
