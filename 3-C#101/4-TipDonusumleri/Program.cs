eb59d3e08aa08742aad7ff1882cecd935852e98e
Bahtiyar Bayir <bhtyr.byr@gmail.com>
2022-04-29 15:37:43 +0300

    C#101 Eğitimi Ders 4 - Tip Dönüşümleri

---
+++ 3-C#101/4-TipDonusumleri/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _4_TipDonusumleri
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Implicit Conversion (bilinçsiz dönüşüm)
+/*
+            Düşük kapasitesi değişkeni üst kapasitesi değişkene atama yapma işlemidir.
+            Atama esnasında hata vermez. Herhangi bir keyword'e ihtiyacı yoktur.
+*/
+
+            //Explicit Conversion (bilinçli dönüşüm)
+
+/*
+            Kendisinden düşük kapasitesi değişkenlere atama yapma işlemidir.
+            Atamanın yapılabilmesi için geliştiricinin müdahalesi gerekir.
+            Örnk;   int x = 4 ;
+                    byte y = (byte)x;
+                    byte y = Convert.ToByte(x);
+                    byte y = byte.Parse(x);
+
+*/
+        }
+    }
+}
