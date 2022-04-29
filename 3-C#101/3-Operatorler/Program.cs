bb96fe483714dc7dbbaeaa4dfe5409e51dcb9ccf
Bahtiyar Bayir <bhtyr.byr@gmail.com>
2022-04-29 15:11:50 +0300

    C#101 Eğitimi Ders 3 - Operatörler

--- 3-C#101/3-Operatorler/Program.cs
+++ 3-C#101/3-Operatorler/Program.cs
@@ -6,7 +6,11 @@ namespace _3_Operatorler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const double meal_cost = 10.25;
+            const int tip_percent = 17;
+            const int tax_percent = 5;
+
+            Console.WriteLine((meal_cost + meal_cost * ((float)tip_percent / 100) + meal_cost * ((float)tax_percent / 100)));
         }
     }
 }
