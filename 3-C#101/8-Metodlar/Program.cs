dd5bffb2dab53c8b22a209a8c7e678e6a49e2e5a
Bahtiyar Bayir <bhtyr.byr@gmail.com>
2022-04-30 23:14:52 +0300

    C#101 Eğitimi Ders 8 - Metodlar

---
+++ 3-C#101/8-Metodlar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _8_Metodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printHelloWorld();
+        }
+        private static void printHelloWorld()
+        {
+            Console.WriteLine("Merhaba Dünya");
+        }
+    }
+}
