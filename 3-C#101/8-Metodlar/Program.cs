0a6e3634da24657dcbd913192ea5c38efbef1ccb
Bahtiyar Bayir <bhtyr.byr@gmail.com>
2022-04-30 23:35:14 +0300

    C#101 Eğitimi Ders 8 - Metodlar

--- 3-C#101/8-Metodlar/Program.cs
+++ 3-C#101/8-Metodlar/Program.cs
@@ -11,6 +11,21 @@ namespace _8_Metodlar
         private static void printHelloWorld()
         {
             Console.WriteLine("Merhaba Dünya");
+
+            Matematikselİslemler mi = new Matematikselİslemler();
+            Console.WriteLine(mi.topla(1,2));
+            Console.WriteLine(mi.topla("1","2"));
         }
+
+    }
+    class Matematikselİslemler
+    {
+        public int topla(int x, int y){
+            return x+y;
+        }
+        public int topla(string x, string y){
+            return Convert.ToInt32(x) + Convert.ToInt32(y);
+        }
+
     }
 }
