a6e9f13297c4359c164257855c81b6a26945df21
Bahtiyar Bayir <bhtyr.byr@gmail.com>
2022-04-30 16:35:41 +0300

    C#101 Eğitimi Ders 6 - Karar Yapıları

---
+++ 3-C#101/6-KararYapilari/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _6_KararYapilari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(DateTime.Now.Hour < 6 && DateTime.Now.Hour >18){
+                Console.WriteLine("İyi geceler");
+            }else{
+                Console.WriteLine("İyi günler");
+            }
+        }
+    }
+}
