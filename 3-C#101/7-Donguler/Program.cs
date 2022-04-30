15bd87a0255866d1a95ca5593ba060f2712f348c
Bahtiyar Bayir <bhtyr.byr@gmail.com>
2022-04-30 17:26:07 +0300

    C#101 Eğitimi Ders  7 - Döngüler

--- 3-C#101/7-Donguler/Program.cs
+++ 3-C#101/7-Donguler/Program.cs
@@ -6,20 +6,48 @@ namespace _7_Donguler
     {
         static void Main(string[] args)
         {
-            int sayac = int.Parse(Console.ReadLine());
-            int ct = 0;
-            int tt = 0;       
-            for (int i = 0; i < sayac; i++)
+            ders2while();   
+            int[] list = {1,2,3,4,5,6,7,8,9,0};
+            ders3foreach(list);
+        }
+
+        static void ders3foreach(int[] list){
+            foreach (var item in list)
             {
-                if (i % 2 == 1)
-                {
-                    Console.WriteLine(i);
-                    tt += i;
-                }else{
-                    ct += i;
+                Console.WriteLine(item);
+            }
+        }
+
+        static void ders2while(){
+            int n = int.Parse(Console.ReadLine());
+            int i = -1;
+            int tot = 0;
+            float ort = 0;
+            while(true){
+                i++;
+                tot += i;
+                Console.WriteLine(tot);
+                if(i == n)
+                    break;
+                else{
                 }
             }
-            Console.WriteLine("Çift Toplam " + ct + " Tek Toplam " + tt);
+            ort = (float)tot / i;
+            Console.WriteLine("Girilen sayi " + n + "\nToplam " + tot + "\nOrt " + ort);
+        }
+        static void hackerRank(){
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++){
+
+                for(int j = 0; j < (n - (i + 1)); j++){
+                    Console.Write(" ");
+                }
+
+                for(int k = 0; k < (i + 1); k++){
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
