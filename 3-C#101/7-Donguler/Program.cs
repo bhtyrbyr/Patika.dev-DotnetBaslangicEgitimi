b632b9dbf1cdb1dcd68d3ad729428939654ea4f2
Bahtiyar Bayir <bhtyr.byr@gmail.com>
2022-04-30 17:00:15 +0300

    C#101 Eğitimi Ders  7 - Döngüler

---
+++ 3-C#101/7-Donguler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _7_Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = int.Parse(Console.ReadLine());
+            int ct = 0;
+            int tt = 0;       
+            for (int i = 0; i < sayac; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                    tt += i;
+                }else{
+                    ct += i;
+                }
+            }
+            Console.WriteLine("Çift Toplam " + ct + " Tek Toplam " + tt);
+        }
+    }
+}
