59b24e388e0dbf65e1114fa36f840a2d471bb82f
Bahtiyar Bayir <bhtyr.byr@gmail.com>
2022-04-30 15:48:03 +0300

    C#101 Eğitimi Ders 5 - Hata Yönetimi

---
+++ 3-C#101/5-HataYonetimi/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _5_HataYonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yaşınızı girin>");
+            int yas = yaskontroledici(Console.ReadLine());
+            Console.WriteLine(yas);
+            
+        }
+
+        private static int yaskontroledici(string yas)
+        {
+            int yas_int;
+            try
+            {
+                yas_int = int.Parse(yas);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Hata!\nYas değeri bos olamaz");
+                Console.WriteLine(ex);
+                return 0;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Hata!\nYas değeri sadece sayılardan oluşabilir");
+                Console.WriteLine(ex);
+                return 0;
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Hata!\nÇok büyük veya çok küçük değer girdiniz");
+                Console.WriteLine(ex);
+                return 0;
+            }
+                return yas_int;
+            
+        }
+    }
+}
