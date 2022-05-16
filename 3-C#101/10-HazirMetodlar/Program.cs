2f4a3baf2d0e2cf9981b4d1c42e310f6c95da6b7
Bahtiyar Bayir <bhtyr.byr@gmail.com>
2022-05-16 12:32:05 +0300

    C#101 Eğitimi Ders 10 - Hazır String Metotları

--- 3-C#101/10-HazirMetodlar/Program.cs
+++ 3-C#101/10-HazirMetodlar/Program.cs
@@ -37,10 +37,65 @@ namespace _10_HazirMetodlar
 
             Console.WriteLine(degisken1.Replace("CSharp","C#"));
             Console.WriteLine(degisken1.Replace(" ","*"));
-*/
+
             Console.WriteLine(degisken1.Split(" ")[1]);
 
-            Console.WriteLine(degisken1.Substring(4,6));        
+            Console.WriteLine(degisken1.Substring(4,6));  
+
+            int count = Convert.ToInt16(Console.ReadLine());
+            string[] words = new string[count];
+            
+            for(int i = 0; i < count; i++){
+            
+            } 
+    
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine(DateTime.Now.DayOfYear);
+
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+            Console.WriteLine(DateTime.Now.AddDays(2));
+
+            //Datetime Format
+
+            Console.WriteLine(DateTime.Now.ToString("dd"));
+            Console.WriteLine(DateTime.Now.ToString("ddd"));
+            Console.WriteLine(DateTime.Now.ToString("dddd"));
+            Console.WriteLine(DateTime.Now.ToString("MM"));
+            Console.WriteLine(DateTime.Now.ToString("MMM"));
+            Console.WriteLine(DateTime.Now.ToString("MMMM"));
+*/
+            Console.WriteLine(Math.Abs(-25));
+            Console.WriteLine(Math.Sin(10));
+            Console.WriteLine(Math.Cos(10));
+            Console.WriteLine(Math.Tan(10));
+
+            Console.WriteLine(Math.Ceiling(22.3));  // 23
+            Console.WriteLine(Math.Round(22.3));    // 22
+            Console.WriteLine(Math.Round(22.7));    // 23
+            Console.WriteLine(Math.Floor(22.7));    // 22
+
+            Console.WriteLine(Math.Min(-2,6));      // -2
+            Console.WriteLine(Math.Max(-2,6));      //  6
+
+            Console.WriteLine(Math.Pow(3,4));       //  81
+            Console.WriteLine(Math.Sqrt(4));        //  2
+            Console.WriteLine(Math.Log(9));         // 9 un e tabanındaki logaritmik karşılığını verir.
+            Console.WriteLine(Math.Exp(3));         // e üzeri 3 ü verir.
+            Console.WriteLine(Math.Log10(10));      //  10 sayısının logaritma 10 tabanındaki karşılğı.
+
         }
     }
 }
