07c1f4f4dd87a88890d86d1cc9f677a13e3d4a9a
Bahtiyar Bayir <bhtyr.byr@gmail.com>
2022-05-16 13:55:48 +0300

    C#101 Eğitimi Ders 13 - Dictionary

---
+++ 3-C#101/13-Dictionary/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _13_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> kullanicilar = new Dictionary<int, string>();
+
+            kullanicilar.Add(10,"Bahtiyar BAYIR");
+            kullanicilar.Add(12,"Ziya Başkesen");
+            kullanicilar.Add(13,"Özgan KİM");
+            //Erişim
+            Console.WriteLine("********** elemanlara erişim ************");
+
+            Console.WriteLine(kullanicilar[12]);
+            foreach (var item in kullanicilar)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Count
+            Console.WriteLine(kullanicilar.Count);
+
+            //Contains
+
+            Console.WriteLine(kullanicilar.ContainsKey(12));
+            Console.WriteLine(kullanicilar.ContainsValue("Özgan KİM"));
+
+            //Remove
+            Console.WriteLine("**************remove**************");
+            kullanicilar.Remove(12);
+            foreach (var item in kullanicilar)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Remove
+            Console.WriteLine("**************Key**************");
+            
+            foreach (var item in kullanicilar.Keys)
+            {
+                Console.WriteLine(item);
+            }
+            //Remove
+            Console.WriteLine("**************Value**************");
+            
+            foreach (var item in kullanicilar.Values)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
