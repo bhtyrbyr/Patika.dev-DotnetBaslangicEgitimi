d276072e05fdf1dda38e9eb3cce926dd3f01583c
Bahtiyar Bayir <bhtyr.byr@gmail.com>
2022-05-16 13:41:15 +0300

    C#101 Eğitimi Ders 12 - ArrayList

---
+++ 3-C#101/12-ArrayList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _12_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+
+            liste.Add("Ayşe");
+            liste.Add(2);
+            liste.Add(true);
+            liste.Add('a');
+
+            Console.WriteLine(liste[1]);
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //AddRange
+            Console.WriteLine("************ Add Range *******************");
+            string[] renler = {"Kırmızı","Sarı","Yeşil"};
+            List<int> sayilar = new List<int>(){1,2,3,4,5,6,7,8,9};
+            liste.AddRange(renler);
+            liste.AddRange(sayilar);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Sort
+            Console.WriteLine("******* sort **********");
+
+            liste.Sort();
+        }
+    }
+}
